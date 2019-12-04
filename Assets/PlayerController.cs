1294ed4a7461920d9ea1fedfb391803b71c0213a
sendoakira <58533841+sendoakira@users.noreply.github.com>
2019-12-04 19:53:55 -0300

    Projeto_alien_alterado

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	void Update () {
+		float horizontal = Input.GetAxis ("Horizontal");
+		Vector3 movimento = new Vector3 (horizontal * 5 * Time.deltaTime, 0, 0);
+		transform.Translate (movimento);
+
+	}
+}
